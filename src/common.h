c0f69e93f5081211fb0492b14d9cf8dfac8b41fe
kerat71 <milen.ivanov@abv.bg>
2023-06-18 18:36:55 +0300

    sinc

--- src/common.h
+++ src/common.h
@@ -4,11 +4,11 @@
 #pragma once
 #pragma GCC optimize("Os") // optimize for code size
 
-#define _SSID               "mySSID"                        // Your WiFi credentials here
-#define _PW                 "myWiFiPassword"
+#define _SSID               "kerat"                        // Your WiFi credentials here
+#define _PW                 "7103280468"
 #define TZName              "CET-1CEST,M3.5.0,M10.5.0/3"    // Timezone (more TZNames in "rtime.cpp")
 #define DECODER             1                               // (0)VS1053 , (1)MAX98357A PCM5102A... (2)AC101 (3)ES8388 (4)WM8978
-#define TFT_CONTROLLER      3                               // (0)ILI9341, (1)HX8347D, (2)ILI9486a, (3)ILI9486b, (4)ILI9488
+#define TFT_CONTROLLER      0                               // (0)ILI9341, (1)HX8347D, (2)ILI9486a, (3)ILI9486b, (4)ILI9488
 #define DISPLAY_INVERSION   0                               // (0) off (1) on
 #define TFT_FREQUENCY       40000000                        // 27000000, 40000000, 80000000
 #define TFT_ROTATION        3                               // 1 or 3 (landscape)
@@ -16,7 +16,7 @@
 #define TP_ROTATION         3                               // 1 or 3 (landscape)
 #define AUDIOTASK_CORE      1                               // 0 or 1
 #define AUDIOTASK_PRIO      2                               // 0 ... 24  Priority of the Task (0...configMAX_PRIORITIES -1)
-#define SDMMC_FREQUENCY     20000000                        // 40000000, 2000000, 10000000, not every SD Card will run at 40MHz
+#define SDMMC_FREQUENCY     10000000                        // 40000000, 2000000, 10000000, not every SD Card will run at 40MHz
 #define FTP_USERNAME        "esp32"                         // user and pw in FTP Client
 #define FTP_PASSWORD        "esp32"
 
